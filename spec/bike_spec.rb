945f176e0d6178a9f3247cad60faa0142cd37686
michkles <michkles@gmail.com>
2017-04-01 12:00:00 +0100

    Bike class added, 2 tests for bike passing

---
+++ spec/bike_spec.rb
@@ -0,0 +1,9 @@
+require 'bike'
+
+describe Bike do
+  it "should respond to release_bike method" do
+    expect(subject).to respond_to :working?
+  end
+
+  it { is_expected.to respond_to :working? }
+end
