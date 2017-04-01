afc319df56690b17e16f7b1c267cacc4ba6e6fd7
michkles <michkles@gmail.com>
2017-04-01 14:00:31 +0100

    added exception to release_bike method in DockingStation and test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,15 +2,23 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it "should release a bike" do
-    expect(subject).to respond_to :release_bike
-  end
+  describe '#release_bike' do
+    it "releases a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
 
-  it { is_expected.to respond_to :release_bike } # one-liner syntax
+    it { is_expected.to respond_to :release_bike } # one-liner syntax
 
-  it "releases working bikes" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    it "releases working bikes" do
+      bike = Bike.new
+      expect(bike).to be_working
+    end
+
+    it "raises error when there are no bikes available" do
+      expect { subject.release_bike }.to raise_error "No bikes available"
+    end
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
