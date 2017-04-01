e2dc5905958fcb08e55dda65c360249a29f5bfb5
michkles <michkles@gmail.com>
2017-04-01 13:36:47 +0100

    added bike as instance variable to DockingStation class, 2 passing tests added

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,9 +8,24 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike } # one-liner syntax
 
-
   it "releases working bikes" do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike) }
+
+  it "docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it "returns docked bike" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+
 end
