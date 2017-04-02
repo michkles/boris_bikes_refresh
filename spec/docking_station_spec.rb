7eaffe8818046eb236d4354fda9db999ba57e35e
michkles <michkles@gmail.com>
2017-04-02 12:25:18 +0100

    capacity added as constant in docking station class and test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -37,7 +37,7 @@ describe DockingStation do
     end
 
     it "raises error when full" do
-      20.times { subject.dock Bike.new }
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to  raise_error "Station is full"
     end
   end
